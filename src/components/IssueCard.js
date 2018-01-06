38b639da83cf42a1189e75f23f9413e943fc1d50
Ajaykumar <pajay2507@gmail.com>
2018-01-05 19:41:16 -0500

    added issue card component

---
+++ src/components/IssueCard.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class IssueCard extends Component {
+
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return (
+
+            <div>Issue Card</div>
+
+        )
+    }
+}
+
+export default IssueCard;
--- src/components/IssuesList.js
+++ src/components/IssuesList.js
@@ -1,36 +1,36 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import IssueCard from  './IssueCard';
 
+// Fetch all the open issues for Good First Issue
 class IssuesList extends Component {
 
     constructor(props){
         super(props);
+        this.state = {
+            issueList: []
+        }
     }
 
     componentDidUpdate(prevProps,prevState){
-        console.log("component did update");
-        console.log(prevProps);
-        console.log(this.props);
         if(this.props.language != prevProps.language){
             axios.get("https://api.github.com/search/issues?q=label:%22good+first+issue%22+language:"+this.props.language+"+state:open&sort=created&order=asc")
                 .then(function (response) {
-                    console.log(response);
+
+                    this.setState({
+                        issueList: response.data
+                    })
+
                 })
                 .catch(function (error) {
                     console.log(error);
                 });
         }
     }
-
-    componentDidMount(){
-       console.log("component did mount");
-
-    }
-
     render() {
         return (
             <div>
-                Issue List
+                <IssueCard />
             </div>
         );
     }
